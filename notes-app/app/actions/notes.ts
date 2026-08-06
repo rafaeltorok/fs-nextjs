51d4d484adc919ce0ffddb5d7a04669eecf48737
Rafael Torok <rafaeltorok@gmail.com>
2026-08-06 16:21:05 -0300

    update: add option to revalidate the path to prevent stale cached data during a redirect

--- notes-app/app/actions/notes.ts
+++ notes-app/app/actions/notes.ts
@@ -2,10 +2,13 @@
 
 import { redirect } from "next/navigation"
 import { addNote } from "../services/notes"
+import { revalidatePath } from "next/cache"
 
 export const createNote = async (formData: FormData) => {
   const content = formData.get("content") as string
   const important = formData.get("important") === "on"
+  
   addNote(content, important)
+  revalidatePath("/notes")
   redirect("/notes")
 }
