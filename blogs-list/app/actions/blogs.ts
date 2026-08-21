88cf97915c7d13a13c36b2bae43fc9bb5fe9ac15
rafaeltorok <rafaeltorok@gmail.com>
2026-08-21 17:37:04 -0300

    update: add input validation for the add blog form - include error messages for each form field

--- blogs-list/app/actions/blogs.ts
+++ blogs-list/app/actions/blogs.ts
@@ -5,7 +5,24 @@ import { addBlog, likeBlog } from "../services/blogs";
 import { revalidatePath } from "next/cache";
 import { auth } from "../auth";
 
-export async function createBlog(formData: FormData) {
+interface Errors {
+  title?: string;
+  author?: string;
+  url?: string;
+  year?: string;
+}
+
+interface Values {
+  title?: string;
+  author?: string;
+  url?: string;
+  year?: string;
+}
+
+export async function createBlog(
+  prevState: { errors: Errors, values?: Values },
+  formData: FormData,
+) {
   const session = await auth();
   if (!session) {
     redirect("/login");
@@ -16,6 +33,34 @@ export async function createBlog(formData: FormData) {
   const url = formData.get("url") as string;
   const year = formData.get("year") as string;
 
+  const errors: Errors = {};
+
+  // Validate the Title field
+  if (title?.trim().length < 5) {
+    errors.title = "The blog title must be at least 5 characters long";
+  }
+
+  // Validate the Author field
+  if (author?.trim().length < 5) {
+    errors.author = "The author's name must be at least 5 characters long";
+  }
+
+  // Validate the URL field
+  if (url?.trim().length < 5) {
+    errors.url = "The URL must be at least 5 characters long";
+  }
+
+  // Validate the Year field
+  if (Number(year) < 1 || Number(year) > new Date().getFullYear()) {
+    errors.year = "Invalid year";
+  }
+
+  // Check if any of the error fields contains a value on it
+  if (Object.values(errors).some(val => val !== null && val !== undefined && val !== "")) {
+    return { errors, values: { title, author, url, year }};
+  }
+
+  // If there are no errors, proceed to add the new object to the database
   await addBlog(title, author, url, year);
   revalidatePath("/blogs");
   redirect("/blogs");
--- blogs-list/app/blogs/new/page.tsx
+++ blogs-list/app/blogs/new/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { createBlog } from "@/app/actions/blogs";
-import { auth } from "@/app/auth";
-import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 // CSS styles
 import "../../formRow.css";
+import { useActionState, useEffect } from "react";
 
 // Renders each input row on the form
 function renderRow(
@@ -11,35 +14,54 @@ function renderRow(
   type: string,
   name: string,
   required: boolean,
+  defaultValue: string,
 ) {
   return (
     <div className="form-row">
       <label>{label}</label>
       {required ? (
-        <input type={type} name={name} required />
+        <input type={type} name={name} defaultValue={defaultValue} required />
       ) : (
-        <input type={type} name={name} />
+        <input type={type} name={name} defaultValue={defaultValue} />
       )}
     </div>
   );
 }
 
 // Server component
-export default async function NewBlog() {
-  const session = await auth();
+export default function NewBlog() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  const initialState = {
+    errors: { title: "", author: "", url: "", year: "" },
+    values: { title: "", author: "", url: "", year: "" },
+  };
+  const [state, formAction] = useActionState(createBlog, initialState);
 
-  if (!session) {
-    redirect("/login");
-  }
+  useEffect(() => {
+    if (!session) {
+      router.push("/login");
+    }
+  }, [session, router]);
 
   return (
     <div>
       <h2>Add new blog</h2>
-      <form action={createBlog}>
-        {renderRow("Title", "text", "title", true)}
-        {renderRow("Author", "text", "author", true)}
-        {renderRow("URL", "text", "url", true)}
-        {renderRow("Year", "number", "year", true)}
+      <form action={formAction}>
+        {renderRow("Title", "text", "title", true, state.values?.title)}
+        {state.errors?.title && <span className="notification">{state.errors.title}</span>}
+        
+        {renderRow("Author", "text", "author", true, state.values?.author)}
+        {state.errors?.author && <span className="notification">{state.errors.author}</span>}
+        
+        {renderRow("URL", "text", "url", true, state.values?.url)}
+        {state.errors?.url && <span className="notification">{state.errors.url}</span>}
+        
+        {renderRow("Year", "number", "year", true, state.values?.year)}
+        {state.errors?.year && <span className="notification">{state.errors.year}</span>}
+
+        <br />
+        <br />
 
         <button type="submit">Add</button>
       </form>
