c76e359d657ad2e35a436e8fa95f53bb02e0b422
Rafael Torok <rafaeltorok@gmail.com>
2026-08-21 21:40:04 -0300

    update(ui): implement input validation on the register form

--- blogs-list/app/actions/users.ts
+++ blogs-list/app/actions/users.ts
@@ -4,15 +4,65 @@ import { redirect } from "next/navigation";
 import bcrypt from "bcryptjs";
 import { db } from "@/db";
 import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
-export const registerUser = async (formData: FormData) => {
+interface Errors {
+  username?: string;
+  name?: string;
+  password?: string;
+}
+
+interface Values {
+  username?: string;
+  name?: string;
+  password?: string;
+}
+
+export const registerUser = async (
+  prevState: { errors: Errors, values?: Values },
+  formData: FormData,
+) => {
   const username = (formData.get("username") as string)?.trim();
   const name = (formData.get("name") as string)?.trim();
   const password = formData.get("password") as string;
+  const confirmPassword = formData.get("password-confirm") as string;
 
+  const errors: Errors = {};
+
+  // Validate the Username field
+  if (username?.trim().length < 4) {
+    errors.username = "The username must be at least 4 characters long";
+  }
+
+  const existingUser = await db.query.users.findFirst({
+    where: eq(users.username, username),
+  });
+
+  if (existingUser) {
+    errors.username = "Username already exists on the database";
+  }
+
+  // Validate the name field
+  if (name?.trim().length < 4) {
+    errors.name = "The name must be at least 4 characters long";
+  }
+
+  // Validate the password field
+  if (password?.trim().length < 4) {
+    errors.password = "The password must be at least 4 characters long";
+  }
+
+  if (password !== confirmPassword) {
+    errors.password = "Passwords do not match";
+  }
+
+  // Check if any of the error fields contains a value on it
+  if (Object.values(errors).some(val => val !== null && val !== undefined && val !== "")) {
+    return { errors, values: { username, name, password } };
+  }
+
+  // If there are no errors, proceed to add the new user into the database
   const passwordHash = await bcrypt.hash(password, 10);
-
   await db.insert(users).values({ username, name, passwordHash });
-
   redirect("/login");
 }
--- blogs-list/app/register/page.tsx
+++ blogs-list/app/register/page.tsx
@@ -1,23 +1,47 @@
+"use client";
+
 import { registerUser } from "../actions/users";
+import { useActionState } from "react";
+
 import "../formRow.css";
 
+function renderRow(
+  label: string,
+  id: string,
+  type: string,
+  name: string,
+  defaultValue: string,
+) {
+  return (
+    <div className="form-row">
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type={type} name={name} defaultValue={defaultValue} required />
+    </div>
+  );
+}
+
 export default function RegisterPage() {
+  const initialState = {
+    errors: { username: "", name: "", password: "" },
+    values: { username: "", name: "", password: "" },
+  };
+  const [state, formAction] = useActionState(registerUser, initialState);
+
   return (
     <div>
       <h2>Register</h2>
-      <form action={registerUser}>
-        <div className="form-row">
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" name="username" required />
-        </div>
-        <div className="form-row">
-          <label htmlFor="name">Name</label>
-          <input id="name" type="text" name="name" required />
-        </div>
-        <div className="form-row">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required />
-        </div>
+      <form action={formAction}>
+        {renderRow("Username", "username", "text", "username", state.values?.username)}
+        {state.errors?.username && <span className="notification">{state.errors.username}</span>}
+
+        {renderRow("Name", "name", "text", "name", state.values?.name)}
+        {state.errors?.name && <span className="notification">{state.errors.name}</span>}
+        
+        {renderRow("Password", "password", "password", "password", state.values?.password)}
+        {renderRow("Confirm password", "password-confirm", "password", "password-confirm", "")}
+        {state.errors?.password && <span className="notification">{state.errors.password}</span>}
+
+        <br />
 
         <button type="submit">Register</button>
       </form>
