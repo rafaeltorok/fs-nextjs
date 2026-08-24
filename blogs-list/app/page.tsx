6dd37610cff07a050193b3503dac57a2ff53e6da
rafaeltorok <rafaeltorok@gmail.com>
2026-08-24 15:28:37 -0300

    update(ui): implement Tailwind to customize the page style

--- blogs-list/app/page.tsx
+++ blogs-list/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Blogs List</h1>
+    <div className="max-w-xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Blogs List</h1>
       <p>
         Developed by <i>Rafael G. Torok</i> for the{" "}
         <i>FullStackOpen: Next.js</i> course.
