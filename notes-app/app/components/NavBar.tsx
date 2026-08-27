b42b387d1443172eb64e2c621b5745784164a35b
rafaeltorok <rafaeltorok@gmail.com>
2026-08-27 15:51:05 -0300

    update(ui): add Hamburger menu for the mobile version of the NavBar - update(ui): add the currently logged in user to the mobile NavBar - update(ui): capitalize all of the NavBar items for consistency

--- notes-app/app/components/NavBar.tsx
+++ notes-app/app/components/NavBar.tsx
@@ -1,35 +1,109 @@
 "use client";
 
+import { useState } from "react";
 import { useSession, signOut } from "next-auth/react";
 import NavLink from "./NavLink";
 
 export default function NavBar() {
   const { data: session } = useSession();
+  const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
 
   return (
     <nav className="bg-gray-800 text-white px-6 py-3 flex items-center gap-4 overflow-auto">
-      <NavLink href="/">home</NavLink>
-      <NavLink href="/notes">notes</NavLink>
-      <NavLink href="/users">users</NavLink>
-      <div className="ml-auto flex items-center gap-4">
-        {session ? (
-          <>
-            <NavLink href="/me">me</NavLink>
-            <NavLink href="/notes/new">create new</NavLink>
-            <em className="text-gray-300">{session.user?.name} logged in</em>
-            <button
-              onClick={() => signOut()}
-              className="bg-gray-600 hover:bg-gray-500 px-3 py-1 rounded text-sm"
+      <section className="MOBILE-MENU flex lg:hidden w-full">
+        <div
+          className="HAMBURGER-ICON space-y-2"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          <span className="block h-0.5 w-8 bg-gray-500"></span>
+          <span className="block h-0.5 w-8 bg-gray-500"></span>
+          <span className="block h-0.5 w-8 bg-gray-500"></span>
+        </div>
+
+        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+          <div
+            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+            onClick={() => setIsNavOpen(false)}
+          >
+            <svg
+              className="h-8 w-8 text-gray-600"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              logout
-            </button>
-          </>
-        ) : (
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
+          </div>
+          
+          <div
+            className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]"
+            onClick={() => setIsNavOpen(false)}
+          >
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/notes">Notes</NavLink>
+            <NavLink href="/users">Users</NavLink>
+
+            {session ? (
+              <>
+                <NavLink href="/me">Me</NavLink>
+                <NavLink href="/notes/new">Create note</NavLink>
+               
+                <button
+                  onClick={() => signOut()}
+                  className="bg-gray-600 hover:bg-gray-500 px-3 py-1 rounded text-l"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <NavLink href="/login">Login</NavLink>
+                <NavLink href="/register">Register</NavLink>
+              </>
+            )}
+          </div>
+        </div>
+
+        {session && (
           <>
-            <NavLink href="/login">login</NavLink>
-            <NavLink href="/register">register</NavLink>
+            <em className="text-gray-300 ml-auto">
+              {session.user?.name} logged in
+            </em>
           </>
         )}
+      </section>
+
+      <div className="DESKTOP-MENU hidden lg:flex items-center w-full">
+        <div className="space-x-8">
+            <NavLink href="/">Home</NavLink>
+            <NavLink href="/notes">Notes</NavLink>
+            <NavLink href="/users">Users</NavLink>
+          </div>
+
+          <div className="ml-auto space-x-5">
+            {session ? (
+              <>
+                <NavLink href="/me">Me</NavLink>
+                <NavLink href="/notes/new">Create note</NavLink>
+               
+                <button
+                  onClick={() => signOut()}
+                  className="bg-gray-600 hover:bg-gray-500 px-3 py-1 rounded text-l"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <NavLink href="/login">Login</NavLink>
+                <NavLink href="/register">Register</NavLink>
+              </>
+            )}            
+          </div>
       </div>
     </nav>
   );
