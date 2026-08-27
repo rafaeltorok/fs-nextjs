3c5cff4cbde66238382266eb57a4f3e2655ba467
rafaeltorok <rafaeltorok@gmail.com>
2026-08-27 15:57:01 -0300

    fix(ui): add missing logged in user's name to the desktop NavBar version

--- notes-app/app/components/NavBar.tsx
+++ notes-app/app/components/NavBar.tsx
@@ -89,7 +89,9 @@ export default function NavBar() {
               <>
                 <NavLink href="/me">Me</NavLink>
                 <NavLink href="/notes/new">Create note</NavLink>
-               
+                <em className="text-gray-300">
+                  {session.user?.name} logged in
+                </em>{" "}
                 <button
                   onClick={() => signOut()}
                   className="bg-gray-600 hover:bg-gray-500 px-3 py-1 rounded text-l"
