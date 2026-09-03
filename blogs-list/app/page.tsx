5486ed404dfc764389871130160a66014b8654cb
rafaeltorok <rafaeltorok@gmail.com>
2026-09-03 17:18:38 -0300

    refactor: the homepage into a markdown file - update: add more info to the app homepage

--- blogs-list/app/page.tsx
+++ blogs-list/app/page.tsx
@@ -1,11 +1,9 @@
+import Homepage from "./homepage.mdx";
+
 export default function Home() {
   return (
-    <div className="max-w-xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-4">Blogs List</h1>
-      <p>
-        Developed by <i>Rafael G. Torok</i> for the{" "}
-        <i>FullStackOpen: Next.js</i> course.
-      </p>
+    <div className="markdown max-w-xl mx-auto p-6">
+      <Homepage />
     </div>
   );
 }
