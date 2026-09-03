7a76662e672a5ed473ae623e0378d271029c684f
rafaeltorok <rafaeltorok@gmail.com>
2026-09-03 17:17:59 -0300

    update: implement support for adding markdown pages

--- blogs-list/next.config.ts
+++ blogs-list/next.config.ts
@@ -1,8 +1,16 @@
+import createMdx from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Allows all local devices access to the Next Dev Server
   allowedDevOrigins: ["192.168.0.*:3000", "192.168.0.*", "0.0.0.0", "0.0.0.0:3000"],
+
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMdx({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
